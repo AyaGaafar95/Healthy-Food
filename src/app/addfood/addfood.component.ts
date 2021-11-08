4d188cf59484f859dba18f996dec07d5839eb0c6
Aya <ayagafar88@gmail.com>
2021-11-08 15:15:46 +0200

    form ts

--- src/app/addfood/addfood.component.ts
+++ src/app/addfood/addfood.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { format } from 'path';
 
 @Component({
   selector: 'app-addfood',
   templateUrl: './addfood.component.html',
-  styleUrls: ['./addfood.component.css']
+  styleUrls: ['./addfood.component.css'],
 })
 export class AddfoodComponent implements OnInit {
+  healthyFoodForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    picture: new FormControl('', [Validators.required]),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
