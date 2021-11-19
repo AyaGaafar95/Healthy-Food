0d1ac17e711eadb4c725f18ff0e9c697777dbe0d
Aya <ayagafar88@gmail.com>
2021-11-19 18:22:04 +0200

    edit food

--- src/app/editfood/editfood.component.ts
+++ src/app/editfood/editfood.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { FoodService } from '../services/food.service';
+import { FoodSchema } from '../types';
 
 @Component({
   selector: 'app-editfood',
@@ -9,12 +10,41 @@ import { FoodService } from '../services/food.service';
   styleUrls: ['./editfood.component.css'],
 })
 export class EditfoodComponent implements OnInit {
+  foodToEdit: FoodSchema;
   healthyFoodForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     category: new FormControl('', [Validators.required]),
     picture: new FormControl('', [Validators.required]),
   });
-  constructor(private foodService: FoodService, private router: Router) {}
+  constructor(
+    private foodService: FoodService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(
+      (data: ParamMap) => {
+        this.foodService.getFoodById(data.get('Id')).subscribe(
+          (dataOfFoodWhatWeWantEdit: FoodSchema) => {
+            this.foodToEdit = dataOfFoodWhatWeWantEdit;
+            this.healthyFoodForm.patchValue(this.foodToEdit);
+          },
+          () => {}
+        );
+      },
+      () => {}
+    );
+  }
+  submitedForm() {
+    var newFoodData: FoodSchema = this.healthyFoodForm.value;
+    newFoodData._id = this.foodToEdit._id;
+    console.log(this.healthyFoodForm.value);
+    this.foodService.editFood(this.healthyFoodForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl('/');
+      },
+      () => {}
+    );
+  }
 }
